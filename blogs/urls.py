944bbbf54cedf8e058cd6f63c6fef1a268cb6623
Lamin <lb@feinheit.ch>
2013-10-18 13:31:29 +0200

    paginator fertig

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from blogs import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name = 'index'),
+    url(r'^$', 'blogs.views.listing', name = 'index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name = 'detail'),
 )
--- blogs/views.py
+++ blogs/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from blogs.models import Post 
-
-
-class IndexView(generic.ListView):
-    template_name = 'blogs/index.html'
-    context_object_name = 'latest_post_list'
-    
-    def get_queryset(self):
-        return Post.objects.filter(is_active=True).order_by('-pub_date')[:5]
+from blogs.models import Post
         
         
 class DetailView(generic.DetailView):
     model = Post
     template_name = 'blogs/detail.html'
+    
+    
+def listing(request):
+    post_list = Post.objects.filter(is_active=True)
+    paginator = Paginator(post_list, 5)
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blogs/index.html', {"posts": posts})
     
     
 
