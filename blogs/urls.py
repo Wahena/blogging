63c179e06824cbf08d8028f0ebfc9c48d72b9e78
Lamin <lb@feinheit.ch>
2013-10-22 11:14:41 +0200

    Archivfunktion Year eingefügt

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,5 +6,5 @@ from blogs import views
 urlpatterns = patterns('',
     url(r'^$', 'blogs.views.listing', name = 'index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name = 'detail'),
-    url(r'^(?P<year>\d{4})/$',PostYearArchiveView.as_view(), name = 'post_year_archive'),
+    url(r'^archive/(?P<year>\d{4})/$',views.PostYearArchiveView.as_view(), name = 'post_year_archive'),
 )
--- blogs/views.py
+++ blogs/views.py
@@ -35,4 +35,5 @@ class PostYearArchiveView(YearArchiveView):
     
     
     
+    
 
